65170b4a3023febadcf9785d5c58f5686f17c740
SUBWAYDAI\aymerich_p <aymerich_p@subway.com>
2018-06-18 14:51:30 -0400

    Add Process Title.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const title = process.env.TITLE;
+var title = process.env.TITLE;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
